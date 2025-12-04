20423a496d88e962e7ae31f57df7887c399d677e
Vaibhav Vinod Zine <vzine91@gmail.com>
2025-12-04 08:27:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -93,7 +93,9 @@ st.markdown(
 # ============================================================
 @st.cache_resource
 def load_model():
-    return tf.keras.models.load_model("EfficientNetB0_plant_disease.keras")
+    import keras
+    return keras.models.load_model("plant_model_keras3")
+
 
 model = load_model()
 
@@ -602,6 +604,7 @@ elif st.session_state.page == 'Contact':
         कोणत्याही प्रश्न, तांत्रिक सहाय्य किंवा अभिप्रायासाठी, कृपया वरील माहिती वापरून आमच्याशी संपर्क साधा.
         आम्ही शेतकऱ्यांना सर्वोत्तम कृषी तंत्रज्ञान समाधान देण्यासाठी वचनबद्ध आहोत.
         """)
+
 
 
 
